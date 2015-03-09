760cb7821281b1e803f5aa93de95d87bffc46687
Mike Timm <mtimm@cisco.com>
2015-03-09 14:56:43 -0700

    Update ArayLogger to the latest features of SimpleAciUiLogServer. Update the readme.

--- AryaLogger/AryaLogger.py
+++ AryaLogger/AryaLogger.py
@@ -1,13 +1,61 @@
 #!/usr/bin/env python
 
+import logging
+import os
+import signal
 import socket
+import sys
+import tempfile
 from collections import OrderedDict, namedtuple
 from urlparse import urlparse, ResultMixin, parse_qs
 from StringIO import StringIO
 from argparse import ArgumentParser
-from SimpleAciUiLogServer.SimpleAciUiLogServer import SimpleAciUiLogServer
+from SimpleAciUiLogServer import ThreadingSimpleAciUiLogServer, serve_forever
 from cobra.mit.naming import Dn
-from arya.arya import arya
+from arya import arya
+
+server_cert = """
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQC+oA+hYsF3uBIMt7i1ELfUFnyf4/MKM/Ylmy4yBc0/YhqANXYk
+so3+gAGkgRlv9ODdsFS7KvjzyaT0kjgA3ahDPyvtroAOWsdFdHJvtS4Ek1WI1Bee
+0hNZlTmjQgnjp9ENYl9ImGWghcubJhtse5cJhL9c/hq40do4llZjaaEiCQIDAQAB
+AoGAYbd1K7qPCErAXeqT8aVXRo4cZm4YeSN3y4FH5faZZyNoCE7feCJbrZl6vhQ7
+sOtrldi9JpD5uyaju4d00+TMSoFqnTuAYLg4CEUAkAq2Hgg1EfQfPpC8IgYdR5qQ
+hRu0JArXldch1YLHw8GQGkkZe/cJXiHs/FPjmdUQSsydI50CQQDuEecLrSLjuMpE
+i8xjD9cQxSDTHJFDZttVb08fjaKFJk993TsUR2t/eR92OR5m0CFei/RUyYpUaPbk
+1s3Eau7XAkEAzPtnMMKoGR3qfLqXzfmgLwQA0UbeV8PbxRCkaCnSYcpn0qJH7UtS
+Qjb4X6MPA9bNUnydWFgbPgz4MwKRo0q6HwJAP6DxS6GerZZ6GQ/0NJXLOWQ2fbYo
+7QbUoGT7lMdaJJQ0ssMqQyVDifJpgkOJ6JjAEnD9gJvNKPpU4py2qkSaSQJANngr
+0Jo5XwtDD0fqJPLLbRLsQLBLTxkdoj0s4v0SCahmdGNpJ5ZXUn8W+xryV3vR7bRt
+f1dSTefWYH+zQagO0wJBANlNp79CN7ylgXdrhRVQmBsXHN4G8biUUxMYsfK4Ao/i
+Ga3xtkYLv7OmrtY+Gx6w56Jqxyucaka8VBHK0/7JTLE=
+-----END RSA PRIVATE KEY-----
+-----BEGIN CERTIFICATE-----
+MIID+jCCA2OgAwIBAgIJALUh5RwHQhJoMA0GCSqGSIb3DQEBBQUAMIGvMQswCQYD
+VQQGEwJVUzELMAkGA1UECBMCQ0ExETAPBgNVBAcTCFNhbiBKb3NlMRUwEwYDVQQK
+EwxhcGlpbnNwZWN0b3IxHTAbBgNVBAsTFFNpbXBsZUFjaVVpTG9nU2VydmVyMSow
+KAYDVQQDEyFTaW1wbGVBY2lVaUxvZ1NlcnZlci5hcGlpbnNwZWN0b3IxHjAcBgkq
+hkiG9w0BCQEWD210aW1tQGNpc2NvLmNvbTAgFw0xNTAxMjMwMDI1NDJaGA8zMDE0
+MDUyNjAwMjU0Mlowga8xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTERMA8GA1UE
+BxMIU2FuIEpvc2UxFTATBgNVBAoTDGFwaWluc3BlY3RvcjEdMBsGA1UECxMUU2lt
+cGxlQWNpVWlMb2dTZXJ2ZXIxKjAoBgNVBAMTIVNpbXBsZUFjaVVpTG9nU2VydmVy
+LmFwaWluc3BlY3RvcjEeMBwGCSqGSIb3DQEJARYPbXRpbW1AY2lzY28uY29tMIGf
+MA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+oA+hYsF3uBIMt7i1ELfUFnyf4/MK
+M/Ylmy4yBc0/YhqANXYkso3+gAGkgRlv9ODdsFS7KvjzyaT0kjgA3ahDPyvtroAO
+WsdFdHJvtS4Ek1WI1Bee0hNZlTmjQgnjp9ENYl9ImGWghcubJhtse5cJhL9c/hq4
+0do4llZjaaEiCQIDAQABo4IBGDCCARQwHQYDVR0OBBYEFN2EqumA49KSEPjLLSni
+UtKth4zQMIHkBgNVHSMEgdwwgdmAFN2EqumA49KSEPjLLSniUtKth4zQoYG1pIGy
+MIGvMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExETAPBgNVBAcTCFNhbiBKb3Nl
+MRUwEwYDVQQKEwxhcGlpbnNwZWN0b3IxHTAbBgNVBAsTFFNpbXBsZUFjaVVpTG9n
+U2VydmVyMSowKAYDVQQDEyFTaW1wbGVBY2lVaUxvZ1NlcnZlci5hcGlpbnNwZWN0
+b3IxHjAcBgkqhkiG9w0BCQEWD210aW1tQGNpc2NvLmNvbYIJALUh5RwHQhJoMAwG
+A1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEABPx5cxBNOjWOxZbiRVfpzKac
+MKs4tFNtEmilAY7kvNouGaSl1Yw2fCpGXjstOG0+SxPy34YgeQSVOGQI1KXhd7vk
+nALqxrKiP2rzpZveBkjq5voRpFw2creEXyt76EKQgwRHYJP60Vu3bYnYNoFHdUwE
+TOBaHjC6ZZLRd77dd3s=
+-----END CERTIFICATE-----
+"""
+
 
 class ApicParseResult(namedtuple('ApicParseResult',
                                  'scheme netloc path params query fragment'),
@@ -184,8 +232,8 @@ def process_get(url):
     else:
         cobra_str = "\n# api method {0} not supported yet".format(
             purl.api_method)
-    print "GET URL: {0}".format(url)
-    print "{0}".format(cobra_str)
+    logging_str = "GET URL: {0}\n{1}".format(url, cobra_str)
+    logging.debug(logging_str)
 
 
 def undefined(**kwargs):
@@ -204,31 +252,75 @@ def POST(**kwargs):
     purl = apic_rest_urlparse(url)
     a = arya()
     if purl.api_method != 'mo':
-        print "Unknown api_method in POST: {0}".format(purl.api_method)
+        logging.debug("Unknown api_method in POST: {0}".format(purl.api_method))
         return
 
     #cobra_str = convert_dn_to_cobra(purl.dn_or_class)
     cobra_str = a.getpython(jsonstr=payload)
-    print "POST URL: {0}".format(url)
-    print "POST Payload:\n{0}".format(payload)
-    print "SDK:\n\n{0}".format(cobra_str)
+    logging_str = "POST URL: {0}\nPOST Payload:\n{1}\nSDK:\n\n{2}".format(url,
+                                                                      payload,
+                                                                      cobra_str)
+    logging.debug(logging_str)
 
 
 def EventChannelMessage(**kwargs):
-    #print "ignoring EventChannelMessage\n"
+    #logging.debug("ignoring EventChannelMessage\n")
     # Ignore all Event Channel Messages
     pass
 
 
 def start_server(args):
-    server = SimpleAciUiLogServer(("", args.port),
-                                  logRequests=args.logrequests,
-                                  location=args.location)
+
+    # This is used to store the certificate filename
+    cert = ""
+
+    # Setup a signal handler to catch control-c and clean up the cert temp file
+    # No way to catch sigkill so try not to do that.
+    # noinspection PyUnusedLocal
+    def sigint_handler(sig, frame):
+        if not args.cert:
+            try:
+                os.unlink(cert)
+            except OSError:
+                pass
+        print "Exiting..."
+        sys.exit(0)
+
+    ThreadingSimpleAciUiLogServer.prettyprint = args.nice_output
+    ThreadingSimpleAciUiLogServer.indent = args.indent
+
+    http_server = ThreadingSimpleAciUiLogServer(("", args.port),
+                                                logRequests=args.logrequests,
+                                                location=args.location,
+                                                excludes=args.exclude)
     # register our callback functions
-    server.register_function(POST)
-    server.register_function(GET)
-    server.register_function(undefined)
-    server.register_function(EventChannelMessage)
+    http_server.register_function(POST)
+    http_server.register_function(GET)
+    http_server.register_function(undefined)
+    http_server.register_function(EventChannelMessage)
+
+    if not args.cert:
+        # Workaround ssl wrap socket not taking a file like object
+        cert_file = tempfile.NamedTemporaryFile(delete=False)
+        cert_file.write(server_cert)
+        cert_file.close()
+        cert = cert_file.name
+    else:
+        cert = args.cert
+
+    https_server = ThreadingSimpleAciUiLogServer(("", args.sslport),
+                                                 cert=cert,
+                                                 location=args.location,
+                                                 logRequests=args.logrequests,
+                                                 excludes=args.exclude)
+
+    # register our callback functions
+    https_server.register_function(POST)
+    https_server.register_function(GET)
+    https_server.register_function(undefined)
+    https_server.register_function(EventChannelMessage)
+
+    signal.signal(signal.SIGINT, sigint_handler)  # Or whatever signal
 
     # This simply sets up a socket for UDP which has a small trick to it.
     # It won't send any packets out that socket, but this will allow us to
@@ -237,11 +329,11 @@ def start_server(args):
     # and easy copy/paste in the APIC UI.
     ip = [(s.connect((args.apicip, 80)), s.getsockname()[0], s.close()) for s
           in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-    print "serving at:"
-    print "http://" + str(ip) + ":" + str(args.port) + args.location
-    print
-    server.serve_forever()
-
+    print("serving at:")
+    print("http://{0}:{1}{2}".format(str(ip), str(args.port), str(args.location)))
+    print("https://{0}:{1}{2}".format(str(ip), str(args.sslport), str(args.location)))
+    print("")
+    serve_forever([http_server, https_server])
 
 def main():
     parser = ArgumentParser('Archive APIC Rest API calls in the PythonSDK ' +
@@ -265,19 +357,45 @@ def main():
                                                  'logs are being sent to, ' +
                                                  'default=/apiinspector',
                         default="/apiinspector", required=False)
+    parser.add_argument('-s', '--sslport', help='Local port to listen on ' +
+                                                ' for ssl connections, ' +
+                                                ' default=8443', default=8443,
+                        type=int, required=False)
+    parser.add_argument('-c', '--cert', help='The server certificate file' +
+                                             ' for ssl connections, ' +
+                                             ' default="server.pem"',
+                        type=str, required=False)
+    parser.add_argument('-e', '--exclude', action='append', nargs='*',
+                        default=[], choices=['subscriptionRefresh',
+                                             'aaaRefresh', 'topInfo'],
+                        help='Exclude certain types of common "noise" queries.')
     parser.add_argument('-r', '--logrequests', help='Log server requests and ' +
                                                     'response codes to ' +
                                                     'standard error',
                         action='store_true', default=False, required=False)
-    parser.add_argument('-u', '--username', help='Username for APIC account ' +
-                                                 'to be pre-populated in ' +
-                                                 'generated code',
-                        required=False, default='admin')
-    parser.add_argument('-pa', '--password', help='Password for APIC account ' +
-                                                  'to be pre-populated in ' +
-                                                  'generated code',
-                        required=False, default='password')
+    parser.add_argument('-n', '--nice-output', help='Pretty print the ' +
+                                                    'response and payload',
+                        action='store_true', default=False, required=False)
+    parser.add_argument('-i', '--indent', help='The number of spaces to ' +
+                                               'indent when pretty ' +
+                                               'printing',
+                        type=int, default=2, required=False)
+    #parser.add_argument('-u', '--username', help='Username for APIC account ' +
+    #                                             'to be pre-populated in ' +
+    #                                             'generated code',
+    #                    required=False, default='admin')
+    #parser.add_argument('-pa', '--password', help='Password for APIC account ' +
+    #                                              'to be pre-populated in ' +
+    #                                              'generated code',
+    #                    required=False, default='password')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - \n%(message)s')
+
+    if args.exclude:
+        # Flatten the list
+        args.exclude = [val for sublist in args.exclude for val in sublist]
 
     start_server(args)
 
--- README.rst
+++ README.rst
@@ -4,3 +4,147 @@ AryaLogger
 
 Use the SimpleAciUiLogServer and arya to convert UI REST API calls to ACI
 Python SDK code.
+
+Install
+-------
+
+Installation of AryaLogger depends on several other modules that are not part
+of the Python standard library.  Here is a basic list of those modules:
+
+1. acicobra - Not yet available on Pypi
+2. arya - Not yet available on Pypi
+3. SimpleAciUiLogServer - available on Pypi
+
+Installing Dependancies
++++++++++++++++++++++++
+
+The two difficult dependancies to install are aray and acicobra.
+
+Installing acicobra
+"""""""""""""""""""
+
+If you have at least APIC version 1.0(3f) installed on your APIC, installation
+of acicobra can be done by simply pointing pip at your APIC's cobra/_downloads
+directory using the --find-links option using one of these two commands:
+
+    pip install -Z --find-links http://apic/cobra/_downloads acicobra
+
+Or:
+
+    pip install -Z --find-links https://apic/cobra/_downloads acicobra
+
+If your APIC has http disabled (the default), you must either use https or
+enable http on the APIC.
+
+If you have an older version of software on your APIC, you can either download
+acicobra from the Cisco.com software download site (APIC specific contract
+required to be tied to the Cisco.com id doing the download) or you can download
+the acicobra egg from the APIC itself and rename it:
+
+    https://developer.cisco.com/media/apicDcPythonAPI_v0.1/install.html
+
+Installing arya
+"""""""""""""""
+
+Arya is currently (as of March 2015) contained in a subdirectory of the ACI
+repo on github.  Installation of arya simply requires you to clone that
+repository, change to the arya directory and run python against arya's setup.py
+with the install option:
+
+    cd some/directory/you/want/
+    git clone https://github.com/datacenter/ACI.git
+    cd ACI/arya
+    python setup.py install
+
+Installing AryaLogger
++++++++++++++++++++++
+
+AryaLogger is available on pypi so installation is fairly straight forward:
+
+    pip install AryaLogger
+
+This should also pull in SimpleAciUiLogServer if you don't have it installed
+already.
+
+Run AryaLogger
+--------------
+
+There are two possible commands to run AryaLogger on your system.  You only need
+to run one of these commands:
+
+    aryalogger
+
+Or:
+
+    AryaLogger
+
+There are additional options available that can be seen via the -h option:
+
+    $ aryalogger -h
+    usage: Archive APIC Rest API calls in the PythonSDK syntax [-h] [-a APICIP]
+                                                               [-po PORT]
+                                                               [-l LOCATION]
+                                                               [-s SSLPORT]
+                                                               [-c CERT]
+                                                               [-e [{subscriptionRefresh,aaaRefresh,topInfo} [{subscriptionRefresh,aaaRefresh,topInfo} ...]]]
+                                                               [-r] [-n]
+                                                               [-i INDENT]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -a APICIP, --apicip APICIP
+                            If you have a multihomed system, where the apic is on
+                            a private network, the server will print the ip
+                            address your local system has a route to 8.8.8.8. If
+                            you want the server to print a more accurate ip
+                            address for the server you can tell it the apicip
+                            address.
+      -po PORT, --port PORT
+                            Local port to listen on, default=8987
+      -l LOCATION, --location LOCATION
+                            Location that transaction logs are being sent to,
+                            default=/apiinspector
+      -s SSLPORT, --sslport SSLPORT
+                            Local port to listen on for ssl connections,
+                            default=8443
+      -c CERT, --cert CERT  The server certificate file for ssl connections,
+                            default="server.pem"
+      -e [{subscriptionRefresh,aaaRefresh,topInfo} [{subscriptionRefresh,aaaRefresh,topInfo} ...]], --exclude [{subscriptionRefresh,aaaRefresh,topInfo} [{subscriptionRefresh,aaaRefresh,topInfo} ...]]
+                            Exclude certain types of common "noise" queries.
+      -r, --logrequests     Log server requests and response codes to standard
+                            error
+      -n, --nice-output     Pretty print the response and payload
+      -i INDENT, --indent INDENT
+                            The number of spaces to indent when pretty printing
+
+For HTTPS, AryaLogger does come with a default certificate file but it should
+not be used for production.  Instead you should create your own certificate file
+using openssl and pass it to the server:
+
+    openssl req -new -x509 -keyout server.pem -out server.pem -days 36500 -nodes
+
+Pass it in to AryaLogger using the -c/--cert option.
+
+Setup your APIC
+---------------
+
+Setting up your APIC to send log messages to AryaLogger is very simple as well.
+When you run AryaLogger it will output the addresses and ports it is listening
+to:
+
+    $ aryalogger
+    serving at:
+    http://10.1.1.100:8987/apiinspector
+    https://10.1.1.100:8443/apiinspector
+
+If you connect to the APIC graphical user interface (GUI) via http, take note of
+the http URL otherwise if you connect to the GUI using https take note of
+the https URL.
+
+Use this URL in the 'Start Remote Logging' pop-up that is available from the 
+'Welcome' menu at the top right of the GUI.
+
+Once you have started remote logging to the URL provided by AryaLogger, when you
+click around the APIC GUI you should see auto generated Cobra SDK code from
+AryaLogger.
+
